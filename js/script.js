c898ef08e8f980dab1dee705910c1cfb0403a72c
omid hoseindoust <129967198+om66id66@users.noreply.github.com>
2023-07-30 02:37:17 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -84,7 +84,7 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   function (request) {
-    buildAndShowHomeHTML(request) [], // ***** <---- TODO: STEP 1: Substitute [...] ******
+    buildAndShowHomeHTML(request) , // ***** <---- TODO: STEP 1: Substitute [...] ******
       }, true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
