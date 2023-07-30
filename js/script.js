ac5db08101f2853db04dc757913fee6de2da0c4f
omid hoseindoust <129967198+om66id66@users.noreply.github.com>
2023-07-31 02:03:23 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,9 +82,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
- function(request) {
       allCategoriesUrl,
-      [buildAndShowHomeHTML]}, // ***** <---- TODO: STEP 1: Substitute [...] ******
+      [buildAndShowHomeHTML], // ***** <---- TODO: STEP 1: Substitute [...] ******
    true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
