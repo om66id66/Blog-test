2edf4ee2a0bf443c948c2ab30528cd84262be2a8
omid hoseindoust <129967198+om66id66@users.noreply.github.com>
2023-08-01 00:22:40 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,11 +15,11 @@ var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
-  "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json , https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/%7Bcategory_short_name%7D.json";
+  "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
-  "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+  "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/{category_short_name}.json";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
