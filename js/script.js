4063194152a84d3d4030eab16cb05d98cbd78b39
omid hoseindoust <129967198+om66id66@users.noreply.github.com>
2023-08-01 00:44:02 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
       allCategoriesUrl,
-      [buildAndShowHomeHTML], // ***** <---- TODO: STEP 1: Substitute [...] ******
+      function (request) {
+    buildAndShowHomeHTML(request)
+  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
    true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -116,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-         var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml, 'randomCategoryShortName', '\''+chosenCategoryShortName+'\''); 
+         var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'"); 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
