0ded4f8e9ec3a419f24e2035d306a435e19fa0e8
omid hoseindoust <129967198+om66id66@users.noreply.github.com>
2023-08-01 19:26:54 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
-  "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+  Access-Control-Allow-Origin: "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
