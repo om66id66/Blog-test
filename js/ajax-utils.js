b675482cdbebfe23461acf23b3ac3bc0211d5327
omid hoseindoust <129967198+om66id66@users.noreply.github.com>
2023-08-04 01:13:44 +0330

    Update ajax-utils.js

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -30,7 +30,7 @@ ajaxUtils.sendGetRequest =
                        responseHandler,
                        isJsonResponse); 
       };
-    request.open("GET", requestUrl, true);
+    request.open("GET", 'data.txt', requestUrl, true);
     request.send(null); // for POST only
   };
 
